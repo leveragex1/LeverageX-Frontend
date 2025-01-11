68c752d9010204e7570d35f98cacb7df2b14d618
leveragex1 <leveragex502@gmail.com>
2025-01-11 13:57:52 +0530

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -30,7 +30,7 @@ function Login() {
         }
         try {
             // const url = `https://deploy-mern-app-1-api.vercel.app/auth/login`;
-            const url = `https://leveragex-frontend.onrender.com/auth/login`;
+            const url = `https://leveragex-backend-ewgf.onrender.com/auth/login`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
